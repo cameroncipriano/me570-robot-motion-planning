5b6df877bb36d3c3daa8cc8d50e18c3b94a67ecf
Cameron Cipriano <cameron.ciprianom@gmail.com>
2021-12-01 15:52:15 -0500

    Finalized homework 3

--- Homework_3/me570_hw3.py
+++ Homework_3/me570_hw3.py
@@ -1,3 +1,6 @@
+"""
+Defines a module to test general methods in other files
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 import me570_geometry as gm
@@ -6,7 +9,7 @@ import me570_potential as pot
 
 def sphere_testCollision():
     """
-    Generates one figure with a sphere (with arbitrary parameters) and  nb_points=100 random points that
+Generates one figure with a sphere (with arbitrary parameters) and  nb_points=100 random points that
 are colored according to the sign of their distance from the sphere (red for negative, green for
 positive). Generates a second figure in the same way (and the same set of points) but flipping the
 sign of the radius  r of the sphere. For each sampled point, plot also the result of the output
@@ -30,7 +33,7 @@ pointsSphere.
     for i, point in enumerate(test_points.T):
         x_point = point[0]
         y_point = point[1]
-        if flag_points[i] == True:
+        if flag_points[i]:
             collision_x.append(x_point)
             collision_y.append(y_point)
         else:
@@ -56,7 +59,7 @@ pointsSphere.
     for i, point in enumerate(test_points.T):
         x_point = point[0]
         y_point = point[1]
-        if flag_points[i] == True:
+        if flag_points[i]:
             collision_x.append(x_point)
             collision_y.append(y_point)
         else:
@@ -70,4 +73,96 @@ pointsSphere.
     plt.show()
 
 
-# Running me570_potential file
+# sphere_testCollision()
+
+attr_pot = pot.Attractive({'shape': 'quadratic', 'x_goal': np.vstack((0, 0))})
+
+f_handle_attr = lambda point: attr_pot.eval(point)
+plt.figure()
+gm.field_plot_threshold(f_handle_attr)
+
+central_path = gm.Sphere(np.vstack((0, 0)), -6, 5)
+central_sphere = pot.RepulsiveSphere(central_path)
+
+plt.show()
+
+f_handle_repulsive = lambda point: central_sphere.eval(point)
+gm.field_plot_threshold(f_handle_repulsive, 6, 200)
+plt.show()
+
+# world = pot.SphereWorld()
+f_handle_total = lambda point: world_potential.eval(point)
+
+# potential_1 = {
+#     'shape': 'quadratic',
+#     'x_goal': np.vstack(world.x_goal[:, 0]),
+#     'repulsive_weight': 0.01
+# }
+# world_potential = pot.Total(world, potential_1)
+
+# plt.figure()
+# gm.field_plot_threshold(f_handle_total, 10, 200)
+# plt.show()
+
+# potential_2 = {
+#     'shape': 'quadratic',
+#     'x_goal': np.vstack(world.x_goal[:, 1]),
+#     'repulsive_weight': 0.01
+# }
+
+# world_potential = pot.Total(world, potential_2)
+
+# plt.figure()
+# gm.field_plot_threshold(f_handle_total, 10, 200)
+# plt.show()
+
+# potential_3 = {
+#     'shape': 'conic',
+#     'x_goal': np.vstack(world.x_goal[:, 0]),
+#     'repulsive_weight': 0.01
+# }
+
+# world_potential = pot.Total(world, potential_3)
+
+# plt.figure()
+# gm.field_plot_threshold(f_handle_total, 10, 200)
+# plt.show()
+
+# potential_4 = {
+#     'shape': 'conic',
+#     'x_goal': np.vstack(world.x_goal[:, 1]),
+#     'repulsive_weight': 0.01
+# }
+
+# world_potential = pot.Total(world, potential_4)
+
+# plt.figure()
+# gm.field_plot_threshold(f_handle_total, 10, 200)
+# plt.show()
+
+# my_potential_planner = pot.Planner()
+# my_potential_planner.run_plot()
+
+# world = pot.SphereWorld()
+# goal_loc = world.x_goal
+# potential = {
+#     'x_goal': np.vstack(world.x_goal[:, 1]),
+#     'shape': 'quadratic',
+#     'repulsive_weight': 35
+# }
+# f_handle = lambda point: pot.clfcbf_control(point, world, potential)
+# gm.field_plot_threshold(f_handle, 10, 20)
+# world.plot()
+# plt.show()
+
+# total_potential = pot.Total(world, potential)
+# total_pot_handle = lambda point: total_potential.eval(point)
+# total_pot_grad_handle = lambda point: -total_potential.grad(point)
+
+# plt.figure()
+# gm.field_plot_threshold(total_pot_handle, 10, 200)
+
+# plt.figure()
+# gm.field_plot_threshold(total_pot_grad_handle, 10, 30)
+
+# plt.show()
